7fffeb5c7186756f25beae802d8a488390e6af9e
Ivan Xue <xue.ziheng@gmail.com>
2019-08-08 21:43:02 +1000

    add jasmine spec reporter

--- config.js
+++ config.js
@@ -1,32 +1,42 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
+var jasmineReporters = require('jasmine-reporters');
+const { SpecReporter } = require('jasmine-spec-reporter');
+
 exports.config = {
-    framework: 'jasmine',
+  framework: 'jasmine',
     // seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['AngularJSTest.js'],
     multiCapabilities: [
-      {
-        browserName: 'chrome',
-        chromeOptions: {
-          args: ["--headless"]
-        }
-      },
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        args: ["--headless"]
+      }
+    },
       // {
       //   browserName: 'firefox',
       //   "moz:firefoxOptions": {
       //     args: ["--headless"]
       //   }
       // }
-    ],
-    onPrepare: function() {
-      jasmine.getEnv().addReporter(
-        new Jasmine2HtmlReporter({
-          takeScreenshots: true,
-          takeScreenshotsOnlyOnFailures: true,
-          savePath: 'target/screenshots'
-        })
-      );
+      ],
+      onPrepare: function() {
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+          consolidateAll: true,
+          savePath: 'target/',
+          filePrefix: 'xmlresults'
+        }));
 
-      browser.manage().window().maximize();
-    }
-  };
+        jasmine.getEnv().addReporter(
+          new Jasmine2HtmlReporter({
+            takeScreenshots: true,
+            takeScreenshotsOnlyOnFailures: true,
+            savePath: 'target/'
+          })
+          );
+
+        browser.manage().window().maximize();
+      }
+    };
